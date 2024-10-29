93f75c07b1aaafaeae2adb8c6b7f6de569814c6a
Danil Shkoda <danilshkoda1998@gmail.com>
2024-10-29 08:43:30 +0200

    refact:
    docker file

--- src/infrastructure/db/connection.js
+++ src/infrastructure/db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const configuration = require("../config/db.config");
 
-const URL = `mongodb://${configuration.username}:${configuration.password}@${configuration.host}:${configuration.port}`;
+const URL = `mongodb://${configuration.username}:${configuration.password}@mongo:${configuration.port}`;
 async function main() {
   try {
     await mongoose.connect(URL);
